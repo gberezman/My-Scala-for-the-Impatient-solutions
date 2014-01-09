02bf29e81643bb8fe8ae530db46127b5ec2fdda0
greg.berezowsky <greg.berezowsky@gmail.com>
2014-01-09 01:47:07 +0000

    Add missing chapter 7, question 2 source

---
+++ src/Chapter7-Question2a.scala
@@ -0,0 +1,7 @@
+package com.rapidascent {
+
+    class String(value: java.lang.String) {
+        override def toString() = "your string is " + value
+    }
+
+}
---
+++ src/Chapter7-Question2b.scala
@@ -0,0 +1,9 @@
+package com.rapidascent.ch7 {
+
+    object WTF extends App {
+        val myString = new com.rapidascent.String("my string")
+        Console.println(myString.toString)
+    } 
+
+}
+
